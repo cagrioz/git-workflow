26a083ddeb36e66e3ee50bfeb387dc624cdffd28
Umut Baybece <77975246+Umut501@users.noreply.github.com>
2023-05-22 17:09:19 +0300

    header navigation test added

--- components/Header/__tests__/Header.test.tsx
+++ components/Header/__tests__/Header.test.tsx
@@ -1,10 +1,14 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import Header from "../";
+// Using React testing library & Jest
+// Jest provides the describe and test functions 
 
 describe("<Header />", () => {
-    test("renders logo", () => {
-        render(<Header loggedIn={false} />);
-        const logo = screen.getByAltText("Logo");
-        expect(logo).toBeInTheDocument();
-    });
+  test("renders the logo when the user is not logged in", () => {
+    render(<Header loggedIn={false} />);
+    
+    // Verifying that the logo is rendered
+    const logo = screen.getByAltText("Logo");
+    expect(logo).toBeInTheDocument();
+  });
 });
